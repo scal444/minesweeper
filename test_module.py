622e0fd499e0b40a96fe41cd1662b80d7edf0ca7
Kevin Boyd <kevin.boyd@uconn.edu>
2018-05-04 19:12:49 -0400

    set up test module, haven't populated yet

---
+++ test_module.py
@@ -0,0 +1,20 @@
+import unittest
+from ms_board import ms_board
+from ms_button import ms_button
+
+
+class testMSButtonMethods(unittest.TestCase):
+
+    def test_blank_intitiation(self):
+        pass
+
+    def test_populate_board(self):
+        pass
+
+class testMSBoardMethods(unittest.TestCase):
+    pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
