b521fa79663fc2e64281886df93851477845a372
Kevin Boyd <kevin.boyd@uconn.edu>
2019-06-07 20:51:42 -0400

    More groundwork, and a readme that consists of design thoughts

---
+++ tests/context.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# Thanks to https://github.com/kennethreitz for this one
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import sample
